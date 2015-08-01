931f91ad638bd1c0693e0b308bd4f66d2ddaee4f
George Toothman <george.toothman@gmail.com>
2015-07-31 22:02:12 -0400

    pass tests

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  badges.flatten
+end
+
+def assign_rooms(attendees)
+  room_assignments.flatten
+end
+
+def printer(attendees)
+  badges_and_room_assignments.each_line do |text|
+    puts text.chomp
+  end
+end
